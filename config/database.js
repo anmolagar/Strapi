d0e3da0aed726657446736a0234c5ae6d38ffa46
PrateekPo <prateek@codemaya.com>
2023-01-02 18:18:03 +0530

    commit

--- config/database.js
+++ config/database.js
@@ -1,25 +1,17 @@
-switch (process.env.INSTANCE) {
-  case 'staging':
-    console.log('this is staging');
-    module.exports = ({ env }) => ({
-      connection: {
-        client: 'postgres',
-        connection:env('DATABASE_URL', 'postgres://postgres:qwerty@localhost:5433/moodiday-prod'),
-        debug: false,
-      },
-    });
-    
-    break;
-    case 'prod':
-      console.log('this is prod');
-      module.exports = ({ env }) => ({
-        connection: {
-          client: 'postgres',
-           connection:env('DATABASE_URL', 'postgres://postgres:qwerty@localhost:5433/moodiday-prod'),
-          debug: false,
-        },
-      });
+var pg = require('pg');
+pg.defaults.ssl = true;
 
-  default:
-    break;
-}
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi"),
+      user: env("DATABASE_USERNAME", ""),
+      password: env("DATABASE_PASSWORD", ""),
+    },
+
+    useNullAsDefault: false,
+  },
+});
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,23 +1,54 @@
+// module.exports =({env})=>[
+//   'strapi::errors',
+//   'strapi::security',
+//   'strapi::cors',
+//   'strapi::poweredBy',
+//   'strapi::logger',
+//   {
+//     name: 'strapi::cors',
+//     config: {
+//       enabled: true,
+//       headers: '*',
+//       origin: ['https://plankton-app-tmhr6.ondigitalocean.app','http://localhost:3000']
+//     }
+//   },
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+//   {
+//     name: "strapi::security",
+//     config: {
+//       contentSecurityPolicy: {
+//         useDefaults: true,
+//         directives: {
+//           "connect-src": ["'self'", "https:"],
+//           "img-src": ["'self'", "data:", "blob:", 'dl.airtable.com', `https://moodiday.${env("DO_SPACE_ENDPOINT")}`],
+//           "media-src": ["'self'", "data:", "blob:",  'dl.airtable.com',`https://moodiday.${env("DO_SPACE_ENDPOINT")}`],
+//           upgradeInsecureRequests: null,
 
-module.exports =({env})=>[
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['https://plankton-app-tmhr6.ondigitalocean.app','http://localhost:3000']
-    }
-  },
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+//         }
+//       },
+//       formidable: {
+//         maxFileSize: 10737418240, // multipart data, modify here limit of uploaded file size
+//       },
+//     }
+//   },
+// ];
+
+// s3 DO
+module.exports = [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::public",
+   "strapi::favicon",
+   "strapi::body",
+
   {
     name: "strapi::security",
     config: {
@@ -25,15 +56,24 @@ module.exports =({env})=>[
         useDefaults: true,
         directives: {
           "connect-src": ["'self'", "https:"],
-          "img-src": ["'self'", "data:", "blob:", 'dl.airtable.com', `https://moodiday.${env("DO_SPACE_ENDPOINT")}`],
-          "media-src": ["'self'", "data:", "blob:",  'dl.airtable.com',`https://moodiday.${env("DO_SPACE_ENDPOINT")}`],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "moodiday-file-storage.s3.us-east-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "moodiday-file-storage.s3.us-east-1.amazonaws.com",
+          ],
           upgradeInsecureRequests: null,
-
-        }
+        },
       },
-      formidable: {
-        maxFileSize: 10737418240, // multipart data, modify here limit of uploaded file size
-      },
-    }
+    },
   },
+  // ...
 ];
